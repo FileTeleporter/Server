3785db502fcb75d551934ba5542fd4454c567421
Jolan Aklin <jolanaklin@gmail.com>
2021-06-15 16:27:41 +0200

    Moved the controller to a separate project

--- FileTeleporterNetController/Constants.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-class Constants
-{
-    public const int TICKS_PER_SEC = 30; // How many ticks per second
-    public const float MS_PER_TICK = 1000f / TICKS_PER_SEC; // How many milliseconds per tick
-}
